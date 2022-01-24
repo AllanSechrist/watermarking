bf7699858e43bb2fca6449daa25342a32654e20c
Allan <sechristallan@gmail.com>
2022-01-24 21:58:11 +0900

    Add ability to click and drag watermark

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
 from tkinter import *
 from menu_functions import MenuManager
-# from PIL import Image, ImageTk
-# from tkinter import ttk
 
-
-# def display_image():
-#     global img
-#     img_to_display = open_image_file()
-#     img = ImageTk.PhotoImage(image = img_to_display)
-#     canvas.create_image(20, 20, anchor=NW, image=img) 
 
 
 root = Tk()
+root.title("WaterMaker Adder")
 
 menu_bar = Menu(root)
 root.config(menu=menu_bar)
 file_menu = Menu(menu_bar, tearoff=False)
 
-canvas = Canvas(root, width = 1260, height = 840)  
-canvas.pack()
-
-menu_manager = MenuManager(root, canvas)
-
+menu_manager = MenuManager(root)
 
 
 # add menu items to the File menu
 file_menu.add_command(label="Open", command=menu_manager.open_image_file)
 file_menu.add_command(label="Save As", command=menu_manager.save_as_file)
+file_menu.add_command(label="Edit Watermark", command=menu_manager.get_user_text)
 
 # add Exit menu option
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.destroy)
-
 menu_bar.add_cascade(label="File", menu=file_menu)
 
-
-# save_btn = Button(root, text ='Save', width=15, height=2, command = save_file)
-# save_btn.pack()
-
-# open_btn = Button(root, text="Open File", width=15, height=2, command=open_image_file).pack()
 
 root.mainloop() 
 
--- menu_functions.py
+++ menu_functions.py
@@ -1,13 +1,24 @@
+from textwrap import fill
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageDraw, ImageFont, ImageTk
+import time
 
+WIDTH = 1260
+HEIGHT = 840
 
 class MenuManager(object):
-    def __init__(self, root, canvas):
+    def __init__(self, root):
         self.root = root
+        canvas = Canvas(root, width = WIDTH, height = HEIGHT)  
+        canvas.pack()
         self.canvas = canvas
+        self.canvas.bind('<B1-Motion>', self.move_watermark)
         self.image_to_save = None
+        self.watermark_text = "Allan's WaterMarker"
+        self.watermark_pos = None
+        self.base_image = None
+        self.overlay = None
 
     def open_image_file(self):
         global img
@@ -17,38 +28,32 @@ class MenuManager(object):
             self.root.title(f'{file_name} - Watermark Adder')
             my_image = Image.open(file_name)
             
-            try:
-                if my_image.width > 1260:
-                    my_image.width = 1260
+            image_width = my_image.width
+            image_height = my_image.height
 
-                if my_image.height > 840:
-                    my_image.height = 840
+            try:
+                if my_image.width > self.canvas.winfo_width(): # get width of the canvas
+                    image_width = self.canvas.winfo_width() # set image width to that of the canvas
+
+                if my_image.height > self.canvas.winfo_height(): # get the height of the canvas
+                    image_height = self.canvas.winfo_height() # set image height to that of the canvas
             except AttributeError:
                 pass
 
             # my_resize = (my_image.width // 4, my_image.height // 4)
-            my_resize = (my_image.width, my_image.height)
-            image_resize = my_image.resize(size=my_resize)
-        
-            image_resize = image_resize.convert("RGBA") 
+            resize = (image_width, image_height)
+            image_resize = my_image.resize(size=resize)
+            image_resize = image_resize.convert("RGBA")
+            self.base_image = image_resize 
+            # image_resize = my_image.thumbnail([self.canvas.winfo_width(), self.canvas.winfo_height()], Image.ANTIALIAS)
 
-            # make a blank image for the text, initialized to transparent text color
-            overlay = Image.new("RGBA", image_resize.size, (255, 255, 255, 0))
-
-            # get a font
-            font = ImageFont.truetype("arial.ttf", 15)
-
-            watermark = ImageDraw.Draw(overlay)
-            watermark.text((image_resize.width - 100, image_resize.height - 50), "Hello World", font=font, fill=(255,255,255,128))
-
-            img_with_watermark = Image.alpha_composite(image_resize, overlay)
-            self.image_to_save = img_with_watermark
-
-            img = ImageTk.PhotoImage(image = img_with_watermark)
-            self.canvas.create_image(20, 20, anchor=NW, image=img) 
-            
-
+            self.update_watermark_overlay(x=self.base_image.width-100, y=self.base_image.height-50)
             my_image.close()   
+
+
+    def move_watermark(self, event):
+        self.update_watermark_overlay(x=event.x, y=event.y)
+
 
     def save_as_file(self):
         files = [("PNG File", "*.png")]
@@ -60,3 +65,58 @@ class MenuManager(object):
             # save the file
             self.image_to_save.save(image_file)
 
+
+    def get_user_text(self):
+        window = Toplevel(self.root)
+        window.geometry("500x250")
+        window.title("Text Watermark")
+
+        label=Label(window, text="Please enter your text into the box", font=("Courier 15 bold"))
+        label.pack(pady=40)
+
+        entry= Entry(window, width= 20)
+        entry.focus_set()
+        entry.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        
+        def set_text(): # sets new watermark
+            # NOT FUNCTIONING AS INTEDED/DOES NOT IMMEDIATLY UPDATE THE WATERMARK ON SCREEN
+            if not self.base_image:
+                self.watermark_text = entry.get()
+                label.configure(text="Your Watermark has been saved!")
+                window.destroy()
+            else:
+                self.watermark_text = entry.get()
+                
+                # self.overlay = Image.new("RGBA", self.base_image.size, (255, 255, 255, 0))
+
+                # # get a font
+                # font = ImageFont.truetype("arial.ttf", 15)
+
+                # self.watermark_pos = ImageDraw.Draw(self.overlay)
+                # self.watermark_pos.text((self.base_image.width - 100, self.base_image.height - 50), f'{self.watermark_text}', font=font, fill=(255,255,255,128), anchor="mb")
+
+
+                # img_with_watermark = Image.alpha_composite(self.base_image, self.overlay)
+                # self.image_to_save = img_with_watermark # store the image to save it.
+
+                # img = ImageTk.PhotoImage(image = img_with_watermark)
+                # self.canvas.create_image(20, 20, anchor=NW, image=img) 
+
+                window.destroy()
+
+        Button(window, text= "Ok",width= 20, command=set_text).pack(pady=40)
+
+
+    def update_watermark_overlay(self, x=0, y=0):
+        global img
+        text_pos = (x, y)
+        self.overlay = Image.new("RGBA", self.base_image.size, (255, 255, 255, 0))
+        # get a font
+        font = ImageFont.truetype("arial.ttf", 15)
+        self.watermark_pos = ImageDraw.Draw(self.overlay)
+        self.watermark_pos.text(text_pos, f'{self.watermark_text}', font=font, fill=(255,255,255,128), anchor="mb")
+        img_with_watermark = Image.alpha_composite(self.base_image, self.overlay)
+        self.image_to_save = img_with_watermark # store the image to save it.
+        img = ImageTk.PhotoImage(image = img_with_watermark)
+        self.canvas.create_image(20, 20, anchor=NW, image=img) 
